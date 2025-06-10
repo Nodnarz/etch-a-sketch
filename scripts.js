139a5654dbd699fe096248da25abc6fd261d2e84
nodnarz <brandonmandras@gmail.com>
2025-06-10 21:20:53 +1000

    Add logic to button and change grid size
    Need to fix the width and height of the cells.
    They should change dynamically to fit the right
    amount on each row.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-const rows = 16;
-const cols = 16;
 const container = document.getElementById("container");
+const reset = document.getElementById("reset");
 
-for (let i = 0; i < cols * rows; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    container.appendChild(cell);
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "gray";
-    })
-}
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "gray";
+    })}} 
 
+reset.addEventListener("click", () => {
+    let newSize = prompt("What size grid do you want");
+    document.querySelectorAll(".cell").forEach(e => e.remove());
+    createGrid(newSize);
+})
+
+createGrid(16);
