4ffb2d42606e2e1398c13f30e52c908ca9f9f2cb
nodnarz <brandonmandras@gmail.com>
2025-06-10 19:54:45 +1000

    Create a 16 x 16 grid of divs inside a container

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const rows = 16;
+const cols = 16;
+const container = document.getElementById("container");
+
+for (let i = 0; i < cols * rows; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+}
