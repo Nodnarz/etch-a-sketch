bcb202b18667f226be789c5dc63c055d9c3e715c
nodnarz <brandonmandras@gmail.com>
2025-06-11 15:01:47 +1000

    Fix cell sizing issue and complete project

--- scripts.js
+++ scripts.js
@@ -2,18 +2,32 @@ const container = document.getElementById("container");
 const reset = document.getElementById("reset");
 
 function createGrid(size) {
+    const containerSize = 600; // Set a fixed size for precise layout
+    container.style.width = `${containerSize}px`; // Set container width
+
+    const cellSize = containerSize / size; // size in px
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
+
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+
         container.appendChild(cell);
         cell.addEventListener("mouseover", () => {
             cell.style.backgroundColor = "gray";
-    })}} 
+        });
+    }
+}
+
 
 reset.addEventListener("click", () => {
-    let newSize = prompt("What size grid do you want");
-    document.querySelectorAll(".cell").forEach(e => e.remove());
-    createGrid(newSize);
+    let newSize = prompt("What size grid do you want? (max: 100)");
+    if (newSize > 0 && newSize <= 100) {
+        document.querySelectorAll(".cell").forEach(e => e.remove());
+        createGrid(newSize);
+    }
+    else alert("Keep the grid size in bounds from 1-100.");
 })
 
 createGrid(16);
